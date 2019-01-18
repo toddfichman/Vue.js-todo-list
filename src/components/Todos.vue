e116c58629f3c73f14d8b8aa82f54a9b127911e0
Todd Fichman <toddfichman@gmail.com>
2019-01-18 11:28:42 -0500

    First vue app (simple todo list)

---
+++ src/components/Todos.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-bind:key="todo.id" v-for="todo in todos"> <!-- v-for is how you loop through array with vue, also need to bind a uniq key -->
+      <TodoItem v-bind:todo="todo" v-on:del-todo="$emit('del-todo', todo.id)"/> <!-- passing up del-todo from TodoItem up to App, with todo.id as parameter -->
+    </div>
+  </div>
+</template>
+
+<script>
+import TodoItem from './TodoItem.vue'
+
+export default {
+  name: 'Todos',
+  components: {
+    TodoItem
+  },
+  props: ['todos'] //getting props from App.vue from v-bind
+}
+</script>
+
+
+<style scoped>
+
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,6 @@
+<template>
+   <div class="about">
+    <h1>About</h1>
+    <p>This is my first vue app</p>
+  </div>
+</template>
